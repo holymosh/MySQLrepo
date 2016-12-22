7b8e20c92bfc8bbc3d2889d9fb8a15ed8c756416
holymosh <skyland7494@mail.ru>
2016-12-22 23:25:44 +0300

    5

---
+++ test.sql
@@ -0,0 +1,2 @@
+INSERT INTO `university`.`museums_company` (`id`, `site`, `name`, `contact`) VALUES ('7', 'www.site7.ru', 'dcscwe', '2');
+INSERT INTO `university`.`schedule` (`id`, `first_day`, `last_day`, `start_time`, `end_time`, `exhibition`) VALUES ('4', '2012-01-20', '2012-01-22', '13:00:00', '14:00:00', '4');
---
+++ trigger.sql
@@ -0,0 +1,62 @@
+
+delimiter ;;
+
+drop trigger if exists `checkTimeSnippet`;;
+create trigger `checkTimeSnippet` before insert on `schedule`
+for each row
+begin
+	if(new.first_day>new.last_day or new.start_time>new.end_time) then
+		call excp();
+	end if;
+end;;
+
+drop trigger if exists `checkTimeSnippet`;;
+create trigger `checkTimeSnippet` before update on `schedule`
+for each row
+begin
+	if(new.first_day>new.last_day or new.start_time>new.end_time) then
+		call excp();
+	end if;
+end;;
+
+drop trigger if exists `deleteExhibition`;;
+create trigger `deleteExhibition` before delete on `exhibition`
+for each row
+begin
+	delete from `schedule` where schedule.id = old.id;
+	delete from `ticket` where ticket.exhibition = old.id;
+end;;
+#sdrop trigger if exists `deleteExhibition`;;
+
+drop trigger if exists `checkUriOnCreate`;;
+create trigger `checkUriOnCreate` before insert on `museums_company`
+for each row
+begin
+	set @uri = new.site;
+    set @locateres = (select locate(".ru",@uri));
+		if(@locateres =0) then
+		call exception();
+		end if;
+end;;
+
+drop trigger if exists `checkContactNumber`;;
+create trigger `checkContactNumber` before insert on `contact`
+for each row 
+begin
+set @contactNumber = new.phone_number;
+	if(@contactNumber<100000) then 
+      call exception();
+      end if;
+end;;
+
+drop trigger if exists `checkOrderMuseums`;;
+create trigger `checkOrderMuseums` before insert on `order`
+for each row
+begin
+set @fromId = new.from;
+set @toId = new.to;
+	if(@fromId = @toId) then 
+		call exception();
+	end if;
+end;;
+delimiter ;
