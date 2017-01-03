8e9b5382dcc57d1af49f92f4db25e10b784ac2d6
holymosh <skyland7494@mail.ru>
2017-01-03 15:35:58 +0300

    uri checker

--- ProcFunc.sql
+++ ProcFunc.sql
@@ -1,36 +1,27 @@
 delimiter ;;
 
-#func1
+#проверка uri
 
 drop function if exists `CheckUri` ;;
 create function `CheckUri` (uri varchar(50))
 returns bool 
 begin
-set @locateres = (select locate(".",uri));
-		if(@locateres =0) then
-			return false;
-		else 
-				return true;
-		end if;
+declare valuesCount int default 0;
+declare currentValue int default 1;
+declare locateResult int default 0;
+declare domenFromTable varchar(10);
+set valuesCount = (select count(*) from `domen`);
+while currentValue <= valuesCount do 
+	set domenFromTable = (select `domen`.`value` from `domen` where currentValue = `domen`.`id`);
+	set currentValue = currentValue + 1 ;
+	set locateResult = (select locate(domenFromTable , uri));
+    if(locateResult > 0 and domenFromTable <> uri) then 
+		return true;
+	end if;
+end while ;
+return false;
 end;; 
 
-#func2
-drop function if exists `CheckDriver` ;;
-create function `CheckDriver` ( id int, snippetBegin datetime , snippetEnd datetime)
-returns bool
-begin 
-set @result = (select count(*) from `order` where id=`order`.driver and (
-(`order`.departuretime<snippetBegin and snippetBegin < `order`.arrivaltime) or 
-(`order`.departuretime<snippetEnd and snippetEnd < `order`.arrivaltime) or
-(snippetBegin<`order`.departuretime and `order`.departuretime<snippetEnd) or
-(snippetBegin<`order`.arrivaltime and `order`.arrivaltime<snippetEnd)));
-
-if(@result >0 ) then
-	  return false;
-	else 
-	  return true;
-	end if;
-end;;
 
 #func3
 drop function if exists `CreateSum` ;;
@@ -67,7 +58,7 @@ end if;
 return result;
 end;;
 
-#func4  
+# Возврат id музея в котором находится экспонта
 drop function if exists `FindExponent` ;; 
 create function `FindExponent` (id int)
 returns int
