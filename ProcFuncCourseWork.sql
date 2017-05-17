89c511485bda9807cb89f9e8a18856cdca5a3493
holymosh <skyland7494@mail.ru>
2017-05-18 02:29:02 +0300

    functions

--- ProcFuncCourseWork.sql
+++ ProcFuncCourseWork.sql
@@ -1,6 +1,6 @@
 delimiter ;;
 
-#проверка uri
+#4 проверка uri
 
 drop function if exists `CheckUri` ;;
 create function `CheckUri` (uri varchar(50))
@@ -21,3 +21,29 @@ while currentValue <= valuesCount do
 end while ;
 return false;
 end;; 
+
+#1 стоимость программыalter
+drop function if exists `CreateProgramPrice` ;;
+create function `CreateProgramPrice`(id int)
+returns int
+begin
+	declare programmersCount int default 0;
+    declare frameworksCount int default 0;
+    declare result int default 0;
+    set frameworksCount = (select count(*) from `program_to_framework` where `program_to_framework`.`programId` = id);
+    set programmersCount = (select count(*) from `programmer_to_program` where `programmer_to_program`.`programId` = id);
+    set result = frameworksCount*15000 + programmersCount*15000;
+    return result;
+end;;
+
+#2 определяем наличие  типа программиста 
+
+drop function if exists `IfCompetenceExists` ;;
+create function `IfCompetenceExists`(competence varchar(50))
+returns boolean 
+begin 
+	declare ifExists boolean default false;
+    set ifExists = (select count(`programer_type`.`type`) from `programer_type` where `programer_type`.`type` = competence)>0;
+    return ifExists;
+end;;
+
